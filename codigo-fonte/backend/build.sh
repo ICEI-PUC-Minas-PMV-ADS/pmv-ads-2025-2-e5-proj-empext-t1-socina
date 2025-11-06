788acfbaf771a1a394d9df6451224dc29cd643ab
Victor Araújo <victorgabrielba00@hotmail.com>
2025-11-05 23:45:50 -0300

    "script para criar usuario admin"

--- codigo-fonte/backend/build.sh
+++ codigo-fonte/backend/build.sh
@@ -5,8 +5,13 @@ set -o errexit
 # Instala as dependências
 pip install -r requirements.txt
 
-# Coleta os arquivos estáticos (CSS, JS, etc.)
+# Coleta os arquivos estáticos
 python manage.py collectstatic --no-input
 
 # Roda as migrações do banco de dados
 python manage.py migrate
+
+# --- NOVO CÓDIGO AQUI ---
+# Cria o superusuário lendo as variáveis de ambiente que você acabou de criar.
+# O '|| true' no final é um truque para o build não falhar se o usuário já existir.
+python manage.py createsuperuser --noinput || true
